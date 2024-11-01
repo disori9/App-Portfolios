d632ba5705e7dabec572894ba8845ece026da001
Ori <disori777@gmail.com>
2024-11-01 11:25:54 +0800

    Add empty column to separate portfolio columns

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with col2:
 
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 data_frame = pandas.read_csv('data.csv', sep=';')
 
 with col3:
