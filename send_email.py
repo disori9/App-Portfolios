37268d3e8eb6e12ae3b179c12bb173402b7f874b
Ori <disori777@gmail.com>
2024-11-03 16:29:15 +0800

    Implement send_email feature into a function, with parameters of user_email and user_message

--- send_email.py
+++ send_email.py
@@ -1,18 +1,23 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(user_email, user_message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "violetori99@gmail.com"
-password = "nsujziuofmbpieno"
+    username = "violetori99@gmail.com"
+    receiver = username
+    password = "nsujziuofmbpieno"
 
-receiver = username
-context = ssl.create_default_context()
-message = """\
-Subject: Hello!
-HI!!!
+    context = ssl.create_default_context()
+
+    message_local = f"""\
+Subject: Porfolio Email
+
+From: {user_email}
+{user_message}
 """
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message_local)
